1d4fcfa10b7a3bfd1dcb5c7060c97515da8b5935
Dan Breczinski <pt2323@gmail.com>
2025-01-28 18:16:00 +0100

    review and update tests

--- test.ts
+++ test.ts
@@ -14,10 +14,12 @@ app.use(express.static(TEST_DIR))
 app.listen(PORT)
 
 const test = async () => {
+  // clear test directory
   console.log('cleaning test dir')
   await fs.remove(TEST_DIR)
   const users: { [key: string]: Messenger } = {}
 
+  // create test users
   for (const letter of ['a', 'b']) {
     console.log(`creating user: ${letter}`)
     await fs.ensureDir(`${TEST_DIR}/${letter}`)
@@ -27,9 +29,11 @@ const test = async () => {
     })
   }
 
+  // get messages before sending
   let messages = await users['a'].getMessages(users['b'].getSelfUrl())
   assert(messages?.length === 0, 'There should be no messages')
 
+  // send messages from at to b message
   await users['a'].sendMessage(
     users['b'].getSelfUrl(),
     new Message({ data: 'hello1' })
@@ -43,10 +47,16 @@ const test = async () => {
     new Message({ data: 'hello3' })
   )
 
+  // confirm messages received
   console.log('getting 3 messages')
   messages = await users['b'].getMessages(users['a'].getSelfUrl())
   assert(messages?.length === 3, 'There should be three messages')
+  console.log(messages)
 
+  // confirm newest message is first
+  assert(messages[0].data == 'hello3', 'The newest message should be first')
+
+  // mrak messages as read
   if (messages?.[0]) {
     console.log('marking message as read')
     await users['b'].markMessageAsRead(users['a'].getSelfUrl(), messages[0])
@@ -74,27 +84,27 @@ const test = async () => {
 
   await users['a'].removeReadMessagesFromQueue(users['b'].getSelfUrl())
 
-  const USER_X_URL = `${URL_BASE}/x`
-  const USER_Y_URL = `${URL_BASE}/y`
+  // const USER_X_URL = `${URL_BASE}/x`
+  // const USER_Y_URL = `${URL_BASE}/y`
 
-  console.log('creating basic string based messengers')
-  const messengerX = new Messenger({
-    selfUrl: USER_X_URL,
-    publicDirRoot: `${TEST_DIR}/x`,
-  })
-  const messengerY = new Messenger({
-    selfUrl: USER_Y_URL,
-    publicDirRoot: `${TEST_DIR}/x`,
-  })
+  // console.log('creating basic string based messengers')
+  // const messengerX = new Messenger({
+  //   selfUrl: USER_X_URL,
+  //   publicDirRoot: `${TEST_DIR}/x`,
+  // })
+  // const messengerY = new Messenger({
+  //   selfUrl: USER_Y_URL,
+  //   publicDirRoot: `${TEST_DIR}/x`,
+  // })
 
-  console.log('sending message')
-  await messengerX.sendMessage(USER_Y_URL, 'hello!')
-  console.log('getting message')
-  messages = await messengerY.getMessages(USER_X_URL)
-  assert(messages.length === 1, 'there should be one message')
-  assert(messages[0].data === 'hello!', 'message should be hello')
+  // console.log('sending message')
+  // await messengerX.sendMessage(USER_Y_URL, 'hello!')
+  // console.log('getting message')
+  // messages = await messengerY.getMessages(USER_X_URL)
+  // assert(messages.length === 1, 'there should be one message')
+  // assert(messages[0].data === 'hello!', 'message should be hello')
 
-  console.log(messages[0])
+  // console.log(messages[0])
 
   console.log('success!!!')
 }
